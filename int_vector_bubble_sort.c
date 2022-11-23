bcda4cdbfb43326235e65eb36b0847453b8d476f
YOYO3333333 <114952052+YOYO3333333@users.noreply.github.com>
2022-11-23 18:57:43 +0100

    Update int_vector_bubble_sort.c

--- int_vector_bubble_sort.c
+++ int_vector_bubble_sort.c
@@ -1,1 +1,28 @@
-
+#include "int_vector_bubble_sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+static struct int_vector swap(struct int_vector vec, size_t i, size_t j)
+{
+    int c = vec.data[i];
+    vec.data[i] = vec.data[j];
+    vec.data[j] = c;
+    return vec;
+}
+struct int_vector int_vector_bubble_sort(struct int_vector vec)
+{
+    if (vec.size = 0)
+        return vec;
+    int sorted = 0;
+    while (!sorted)
+    {
+        sorted = 1;
+        for (size_t i =0; i < vec.size - 1; i++)
+        {
+            if (vec.data[i] > vec.data[i+1])
+            {
+                sorted = 0;
+                swap(vec, i, i + 1);
+            }
+        }
+    }
+}
